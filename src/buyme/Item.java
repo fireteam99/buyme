b42928628f6190926d80531fdcde8f74175c17f9
fireteam99 <raymancheungsy@gmail.com>
2019-04-19 00:05:35 -0400

    created a class named Item which deals with querying, adding, updating, and deleting Auctions/Items

--- src/buyme/Item.java
+++ src/buyme/Item.java
@@ -2,12 +2,11 @@ package buyme;
 import java.sql.*;
 
 public class Item {
-	// allows you to search items while filtering by subcategory and sorting by name, price, and date
-	public ResultSet search(String item_query, String category, int sortNameOption, int sortPriceOption, int sortDateOption) throws SQLException {
-		String selectSQL = "SELECT * FROM Auction WHERE name LIKE ? AND Category = ?";
+	// allows you to search items while filtering by category, subcategory and sorting by name, price, and date
+	public ResultSet search(String item_query, String, category, String subcategory, int sortNameOption, int sortPriceOption, int sortDateOption) throws SQLException {
 		try {
+			String selectSQL = "SELECT * FROM Auction WHERE name LIKE ? AND category LIKE ? AND subcategory LIKE ?";
 			Connection con = DriverManager.getConnection("jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336", "thisisareallysecurepassword551");
-			Statement st = con.createStatement();
 			
 			if (sortNameOption != 0 || sortPriceOption != 0 || sortDateOption != 0) {
 				selectSQL += " ORDER BY";
@@ -32,6 +31,8 @@ public class Item {
 			}
 			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
 			preparedStatement.setString(1, "%"+item_query+"%");
+			preparedStatement.setString(2, "%"+category+"%");
+			preparedStatement.setString(3, "%"+subcategory+"%");
 			ResultSet rs = preparedStatement.executeQuery();
 			return rs;
 		} catch(SQLException se){
@@ -39,15 +40,28 @@ public class Item {
 		} catch(Exception e){
 			throw e;
 		}
-		// retrieves all items in a category
-		public ResultSet getByCategory(String category) {
-			
-		}
-		// retireves all items in a subcategory
-		public ResultSet getBySubcategory(String subcategory) {
-			
-		}
 	}
+	
+	// retrieves all items in a category
+	public ResultSet getByCategory(String category) {
+		String selectSQL = "SELECT * FROM Auction WHERE category = ?";
+		Connection con = DriverManager.getConnection("jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336", "thisisareallysecurepassword551");
+		PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
+		preparedStatement.setString(1, category);
+		ResultSet rs = preparedStatement.executeQuery();
+		return rs;
+	}
+	
+	// retrieves all items in a subcategory
+	public ResultSet getBySubcategory(String subcategory) {
+		String selectSQL = "SELECT * FROM Auction WHERE subcategory = ?";
+		Connection con = DriverManager.getConnection("jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336", "thisisareallysecurepassword551");
+		PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
+		preparedStatement.setString(1, subcategory);
+		ResultSet rs = preparedStatement.executeQuery();
+		return rs;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ItemSearch itemSearch = new ItemSearch();
