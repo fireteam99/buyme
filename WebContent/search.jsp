40108486ca7539aa3c542659ea02eab053b72874
fireteam99 <raymancheungsy@gmail.com>
2019-04-20 23:09:10 -0400

    finished dynamic search for cards

--- WebContent/search.jsp
+++ WebContent/search.jsp
@@ -7,17 +7,21 @@
 <title>Search</title>
 <%@ include file="./partials/commonCss.jsp"%>
 <link rel="stylesheet" href="css/search.css" type="text/css">
+
+<%@page import="buyme.Item"%>
+<%@page import="java.sql.*"%>
+
 </head>
 <body>
 	<div class="content">
 		<%@ include file="./partials/navbar.jsp"%>
 		<%@ include file="./partials/searchBar.jsp"%>
 
-
 		<section>
 			<div class="featured-search-container">
 				<p class="featured-heading hdr-med featured-search">Searching
-					For:</p>
+					For: ${param.search-query}
+				</p>
 			</div>
 			<div class="card sort-filter">
 				<p class="category-heading hdr-sml">Sort</p>
@@ -45,9 +49,65 @@
 					</div>
 				</div>
 			</div>
+			
 			<div class="card item-container">
 				<p class="category-heading hdr-sml">Results</p>
 				<div class="flex-container">
+					<% 
+						// get list of auctions/items
+						Item item = new Item();
+							String sq = request.getParameter("search-query");
+							String cat = request.getParameter("category");
+							String subcat = request.getParameter("subcategory");
+							int sno = Integer.parseInt(request.getParameter("sno"));
+							int spo = Integer.parseInt(request.getParameter("spo"));
+							int sdo = Integer.parseInt(request.getParameter("sdo"));
+						try {
+							ResultSet rs = item.search(sq, cat, subcat, sno, spo, sdo);
+							
+							// print out the items
+							if (!rs.isBeforeFirst()) { // check to see if search is empty
+					%>
+								<p class="body-lrg">No items to show...</p>
+					<%
+								
+							} else {
+								while(rs.next()) {
+									// list out the results
+					%>
+									<div class="item-card featured-item">
+										<div class="item-card-image-container">
+											<img class="item-card-image"
+												src="<%= rs.getString("image") %>"
+												alt="item picture">
+										</div>
+										<div class="item-card-info">
+											<div class="item-card-title-container">
+												<p class="item-card-title hdr-sml"><%= rs.getString("item_name") %></p>
+											</div>
+											<div class="item-card-details-container">
+												<p class="item-card-details body-sml">
+												<%= rs.getString("item_description") %>
+												</p>
+											</div>
+											<div class="item-card-price-container">
+												<p class="hdr-med item-card-price"><%= rs.getString("buy_at_price") %></p>
+											</div>
+											<div class="item-card-btn-container">
+												<a class="btn-details btn-secondary" href="<%= "/item.jsp?auctionID=" + rs.getString("auction_id") %>">More Details</a>
+											</div>
+										</div>
+									</div>
+					<%
+								}
+							}
+							
+						} catch (SQLException se) {
+							response.sendRedirect("../500.jsp");
+						} catch (Exception e) {
+							response.sendRedirect("../500.jsp");
+						}
+					%>
 					<div class="item-card featured-item">
 						<div class="item-card-image-container">
 							<img class="item-card-image"
--- src/buyme/Item.java
+++ src/buyme/Item.java
@@ -7,11 +7,13 @@ public class Item {
 	// sorting by name, price, and date (TESTED)
 	public ResultSet search(String item_query, String category, String subcategory, int sortNameOption,
 			int sortPriceOption, int sortDateOption) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
 			String selectSQL = "SELECT * FROM buyme.Auction WHERE item_name LIKE ? AND category_name LIKE ? AND subcategory_name LIKE ?";
-			Connection con = DriverManager.getConnection(
-					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
-					"thisisareallysecurepassword551");
+			con = DriverManager.getConnection("jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme",
+					"cs336", "thisisareallysecurepassword551");
 
 			if (sortNameOption != 0 || sortPriceOption != 0 || sortDateOption != 0) {
 				selectSQL += " ORDER BY";
@@ -34,84 +36,149 @@ public class Item {
 			} else if (sortNameOption < 0) {
 				selectSQL += " start_date DESC";
 			}
-			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
-			preparedStatement.setString(1, "%" + item_query + "%");
-			preparedStatement.setString(2, "%" + category + "%");
-			preparedStatement.setString(3, "%" + subcategory + "%");
-			ResultSet rs = preparedStatement.executeQuery();
+			ps = con.prepareStatement(selectSQL);
+			ps.setString(1, "%" + item_query + "%");
+			ps.setString(2, "%" + category + "%");
+			ps.setString(3, "%" + subcategory + "%");
+			rs = ps.executeQuery();
 			return rs;
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
 
 	// retrieves all items in a category (TESTED)
 	public ResultSet searchByCategory(String category) throws SQLException {
 		String selectSQL = "SELECT * FROM buyme.Auction WHERE category_name = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
-			Connection con = DriverManager.getConnection(
+			con = DriverManager.getConnection(
 					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
 					"thisisareallysecurepassword551");
-			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
-			preparedStatement.setString(1, category);
-			ResultSet rs = preparedStatement.executeQuery();
+			ps = con.prepareStatement(selectSQL);
+			ps.setString(1, category);
+			rs = ps.executeQuery();
 			return rs;
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
 
 	// retrieves all items in a subcategory (TESTED)
 	public ResultSet searchBySubcategory(String subcategory) throws SQLException {
 		String selectSQL = "SELECT * FROM buyme.Auction WHERE subcategory_name = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
-			Connection con = DriverManager.getConnection(
+			con = DriverManager.getConnection(
 					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
 					"thisisareallysecurepassword551");
-			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
-			preparedStatement.setString(1, subcategory);
-			ResultSet rs = preparedStatement.executeQuery();
+			ps = con.prepareStatement(selectSQL);
+			ps.setString(1, subcategory);
+			rs = ps.executeQuery();
 			return rs;
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
-	
+
 	// retrieves a single item in database by id (TESTED)
 	public ResultSet getByID(int itemID) throws SQLException {
 		String selectSQL = "SELECT * FROM buyme.Auction WHERE auction_id = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
-			Connection con = DriverManager.getConnection(
+			con = DriverManager.getConnection(
 					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
 					"thisisareallysecurepassword551");
-			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
-			preparedStatement.setInt(1, itemID);
-			ResultSet rs = preparedStatement.executeQuery();
+			ps = con.prepareStatement(selectSQL);
+			ps.setInt(1, itemID);
+			rs = ps.executeQuery();
 			return rs;
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
-	
+
 	// creates an item(auction) in database (TESTED)
-	public void create(String itemName, String seller, double buyAtPrice, double increment, String itemDescription, int userID, String subcategoryName, String image, Timestamp endDate, String categoryName) throws SQLException {
-		String insertSQL = "INSERT INTO buyme.Auction" + 
-				"(item_name, current_bid, seller, buy_at_price, increment, auction_id," + 
-				"item_description, user_id, subcategory_name, image, end_date, start_date, category_name)" + 
-				"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	public void create(String itemName, String seller, double buyAtPrice, double increment, String itemDescription,
+			int userID, String subcategoryName, String image, Timestamp endDate, String categoryName)
+			throws SQLException {
+		String insertSQL = "INSERT INTO buyme.Auction"
+				+ "(item_name, current_bid, seller, buy_at_price, increment, auction_id,"
+				+ "item_description, user_id, subcategory_name, image, end_date, start_date, category_name)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		Connection con = null;
+		PreparedStatement ps = null;
 		try {
-			Connection con = DriverManager.getConnection(
+			con = DriverManager.getConnection(
 					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
 					"thisisareallysecurepassword551");
-			PreparedStatement ps = con.prepareStatement(insertSQL);
+			ps = con.prepareStatement(insertSQL);
 			ps.setString(1, itemName);
 			ps.setDouble(2, buyAtPrice);
 			ps.setString(3, seller);
@@ -130,32 +197,52 @@ public class Item {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
-	
+
 	// deletes an item by id
 	public void deleteByID(int itemID) throws SQLException {
 		String deleteSQL = "DELETE FROM buyme.Auction WHERE auction_id = ?";
+		Connection con = null;
+		PreparedStatement ps = null;
 		try {
-			Connection con = DriverManager.getConnection(
+			con = DriverManager.getConnection(
 					"jdbc:mysql://cs336.c7mvfesixgy7.us-east-2.rds.amazonaws.com:3306/buyme", "cs336",
 					"thisisareallysecurepassword551");
-			PreparedStatement preparedStatement = con.prepareStatement(deleteSQL);
-			preparedStatement.setInt(1, itemID);
-			preparedStatement.executeUpdate();
+			ps = con.prepareStatement(deleteSQL);
+			ps.setInt(1, itemID);
+			ps.executeUpdate();
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
 			throw e;
+		} finally {
+			try {
+				ps.close();
+			} catch (Exception e) {
+				/* ignored */ }
+			try {
+				con.close();
+			} catch (Exception e) {
+				/* ignored */ }
 		}
 	}
-		
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Item item = new Item();
 		try {
 			// debugging
-			
+
 			// inserting an item
 			String itemName = "Test Item";
 			String seller = "a";
@@ -167,8 +254,9 @@ public class Item {
 			String image = "https://img.letgo.com/images/a9/c5/6e/fa/a9c56efa672d4ccd4ac5c1e4395b544d.jpeg?impolicy=img_600";
 			Timestamp endDate = new Timestamp(System.currentTimeMillis() + 1000000000);
 			String categoryName = "Electronics";
-			item.create(itemName, seller, buyAtPrice, increment, itemDescription, userID, subcategoryName, image, endDate, categoryName);
-			
+			item.create(itemName, seller, buyAtPrice, increment, itemDescription, userID, subcategoryName, image,
+					endDate, categoryName);
+
 			// searching for items
 //			ResultSet rs = item.search("", "Electronics", "Phones", 0, 0, 0);
 //			ResultSet rs = item.searchByCategory("Electronics");
@@ -184,11 +272,11 @@ public class Item {
 //				}
 //				System.out.println();
 //			}
-			
+
 			// deleting an item
 //			int auctionID = 1;
 //			item.deleteByID(auctionID);
-			
+
 		} catch (SQLException se) {
 			se.printStackTrace();
 		} catch (Exception e) {
